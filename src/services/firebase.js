19ffe0cbd1ad14d4723b63725a2a85f47698268e
jusim@medinatech.co <jusim@medinatech.co>
2023-11-12 17:19:36 +0600

    Bug fixing

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,23 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-	// apiKey: 'AIzaSyBTPib--JiS7phRmxpwcBh2fcMEADViIRM',
-	// apiKey: process.env.REACT_APP_FIREBASE_KEY,
-	// authDomain: 'freshmeals-reactjs.firebaseapp.com',
-	// projectId: 'freshmeals-reactjs',
-	// storageBucket: 'freshmeals-reactjs.appspot.com',
-	// messagingSenderId: '813006076156',
-	// appId: '1:813006076156:web:c5e3a3142536418935bf79',
-	// measurementId: 'G-XKMMT2WXNB',
-
-
-	apiKey: "AIzaSyD1dl9M1m1xkymDBLVi_0aq2DAJbWyVAc0",
-  authDomain: "eshop-a2f38.firebaseapp.com",
-  projectId: "eshop-a2f38",
-  storageBucket: "eshop-a2f38.appspot.com",
-  messagingSenderId: "767127133639",
-  appId: "1:767127133639:web:cacf1f1e44381fc5c44de2",
-	measurementId: 'G-XKMMT2WXNB',
+	apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGGING_SENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
